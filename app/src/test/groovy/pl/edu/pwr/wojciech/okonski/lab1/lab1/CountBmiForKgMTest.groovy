09eb3d82da1225daeae892064a42f61568717cac
Wojtek Okoński <wojteko22.96@gmail.com>
2017-03-17 18:58:23 +0100

    Add Spock tests

--- app/src/test/groovy/pl/edu/pwr/wojciech/okonski/lab1/lab1/CountBmiForKgMTest.groovy
+++ app/src/test/groovy/pl/edu/pwr/wojciech/okonski/lab1/lab1/CountBmiForKgMTest.groovy
@@ -1,29 +1,73 @@
 package pl.edu.pwr.wojciech.okonski.lab1.lab1
 
 import spock.lang.Specification
+import spock.lang.Unroll
+
+import static spock.util.matcher.HamcrestMatchers.closeTo
 
 class CountBmiForKgMTest extends Specification {
-    def "IsMassValid"() {
+    def countBmi = new CountBmiForKgM();
 
-    }
-
-    def "IsHeightValid"() {
-
-    }
-
-    def "CalculateBMI"() {
-
-    }
-
-    def "test"() {
-        def testMass = -17f;
-        def testHeight = 1.85f;
-
+    @Unroll
+    def "verify mass #mass"() {
         when:
-        def countBMITest = new CountBmiForKgM();
-        countBMITest.calculateBMI(testMass, testHeight);
+        def isValid = countBmi.isMassValid(mass)
+
+        then:
+        isValid == expectedResult
+
+        where:
+        mass   || expectedResult
+        10f    || false
+        10.1f  || true
+        249.9f || true
+        250f   || false
+
+    }
+
+    @Unroll
+    def "verify height #height"() {
+        when:
+        def isValid = countBmi.isHeightValid(height)
+
+        then:
+        isValid == expectedResult
+
+        where:
+        height || expectedResult
+        0.5f   || false
+        0.51f  || true
+        2.49f  || true
+        2.5f   || false
+    }
+
+    @Unroll
+    def "throw exception for mass #mass and height #height"() {
+        when:
+        countBmi.calculateBMI(mass, height);
 
         then:
         thrown(IllegalArgumentException)
+
+        where:
+        mass | height
+        10f  | 1.5f
+        250f | 1.5f
+        80f  | 0.5f
+        80f  | 2.5f
+    }
+
+    def "count BMI"() {
+        when:
+        def actual = countBmi.calculateBMI(mass, height)
+
+        then:
+        actual closeTo(expectedResult, acceptableError)
+
+        where:
+        mass = 67f
+        height = 1.85f
+        expectedResult = 19.5f
+        acceptableError = 0.1f
     }
 }
--- app/src/test/groovy/pl/edu/pwr/wojciech/okonski/lab1/lab1/CountTest.groovy
+++
@@ -1,8 +0,0 @@
-package pl.edu.pwr.wojciech.okonski.lab1.lab1
-
-import spock.lang.Specification
-
-class CountTest extends Specification {
-
-
-}
