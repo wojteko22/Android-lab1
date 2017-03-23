f3aa7f0de42e2606d8524d758be19d9ea50d3815
Wojtek Okoński <wojteko22.96@gmail.com>
2017-03-23 20:59:40 +0100

    Add Spinner

--- app/src/main/java/pl/edu/pwr/wojciech/okonski/lab1/lab1/MainActivity.kt
+++ app/src/main/java/pl/edu/pwr/wojciech/okonski/lab1/lab1/MainActivity.kt
@@ -3,7 +3,9 @@ package pl.edu.pwr.wojciech.okonski.lab1.lab1
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import pl.edu.pwr.wojciech.okonski.lab1.lab1.R.array.units
 
 class MainActivity : AppCompatActivity() {
     private val bmiCounter = CountBmiForKgM()
@@ -11,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSpinner()
         setOnClickListener()
     }
 
@@ -22,5 +25,11 @@ class MainActivity : AppCompatActivity() {
             val bmi = bmiCounter.calculateBMI(mass, height).toString()
             tvBmiResult.text = bmi
         }
+    }
+
+    private fun setSpinner() {
+        val adapter = ArrayAdapter.createFromResource(this, units, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
