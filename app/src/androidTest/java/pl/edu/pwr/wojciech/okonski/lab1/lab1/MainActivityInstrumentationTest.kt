e9b078b33ad94bea8abcdddb6e804e2235d3613a
Wojtek Okoński <wojteko22.96@gmail.com>
2017-04-03 08:00:20 +0200

    Correct Espresso tests

--- app/src/androidTest/java/pl/edu/pwr/wojciech/okonski/lab1/lab1/MainActivityInstrumentationTest.kt
+++ app/src/androidTest/java/pl/edu/pwr/wojciech/okonski/lab1/lab1/MainActivityInstrumentationTest.kt
@@ -2,11 +2,9 @@ package pl.edu.pwr.wojciech.okonski.lab1.lab1
 
 import android.support.test.espresso.Espresso.onData
 import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.action.ViewActions.typeText
+import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
@@ -24,6 +22,7 @@ class MainActivityInstrumentationTest {
 
     @Test
     fun displayProperResultsWhenDataIsCorrect() {
+        clearInputFields()
         onView(withId(R.id.etMass)).perform(typeText("67.5"))
         onView(withId(R.id.etHeight)).perform(typeText("1.85"))
         onView(withId(R.id.btnCount)).perform(click())
@@ -32,19 +31,27 @@ class MainActivityInstrumentationTest {
     }
 
     @Test
-    fun displayPromptWhenSomeFieldIsEmpty() {
+    fun disableButtonsWhenSomeInputIsInvalid() {
+        clearInputFields()
         onView(withId(R.id.etMass)).perform(typeText("60"))
-        onView(withId(R.id.btnCount)).perform(click())
-        onView(withId(R.id.tvDescription)).check(matches(withText(R.string.invalidInput)))
+        onView(withId(R.id.etHeight)).perform(typeText("60"))
+        onView(withId(R.id.btnSave)).check(matches(not(isEnabled())))
+        onView(withId(R.id.btnCount)).check(matches(not(isEnabled())))
     }
 
     @Test
-    fun displayPromptForInvalidInputForLbIn() {
+    fun disableButtonsWhenSomeInputIsInvalidInNotDefaultUnits() {
+        clearInputFields()
         onView(withId(R.id.spinner)).perform(click())
         onData(allOf(`is`(instanceOf(String::class.java)), `is`("lb/in"))).perform(click())
         onView(withId(R.id.etMass)).perform(typeText("67.5"))
         onView(withId(R.id.etHeight)).perform(typeText("1.85"))
-        onView(withId(R.id.btnCount)).perform(click())
-        onView(withId(R.id.tvDescription)).check(matches(withText(R.string.invalidInput)))
+        onView(withId(R.id.btnSave)).check(matches(not(isEnabled())))
+        onView(withId(R.id.btnCount)).check(matches(not(isEnabled())))
+    }
+
+    private fun clearInputFields() {
+        onView(withId(R.id.etMass)).perform(clearText())
+        onView(withId(R.id.etHeight)).perform(clearText())
     }
 }
